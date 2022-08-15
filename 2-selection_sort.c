0b14ef487e62c23869a7bfc36b45a3b6c2e96d08
Cindy Muraya <cmuraya12@gmail.com>
2022-08-15 05:17:48 -0700

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of the array
+ *
+ * Desc: prints the array after each time you swap two elements
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, step, min_idx;
+	int tmp;
+
+	for (step = 0; step < size - 1; step++)
+	{
+		min_idx = step;
+		for (i = step + 1; i < size; i++)
+		{
+			if (array[i] < array[min_idx])
+			{
+				min_idx = i;
+			}
+		}
+
+		if (min_idx != step)
+		{
+			tmp = array[step];
+			array[step] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
+}
